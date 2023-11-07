3a9dd12a7a91d374ed1d41bb6f0d30155ef0cd34
Nguyenvanlen2412 <124877679+Nguyenvanlen2412@users.noreply.github.com>
2023-11-07 23:06:07 +0700

    Add files via upload

---
+++ NguyenVanLen--222028281.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int LCS(int a[], int b[], int m, int n) {
+    int dp[m + 1][n + 1];
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (i == 0 || j == 0) {
+                dp[i][j] = 0;
+            } else if (a[i - 1] == b[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[m][n];
+}
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    int a[m], b[n];
+    for (int i = 0; i < m; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    cout << LCS(a, b, m, n) << endl;
+    return 0;
+}
+
